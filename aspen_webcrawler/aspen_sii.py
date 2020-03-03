afa3696b12e2de45c1776a1e18af782b954c8644
Misha Berrien <misha.berrien@gmail.com>
2020-03-03 16:36:21 -0600

    put original files in

---
+++ aspen_webcrawler/aspen_sii.py
@@ -0,0 +1,13 @@
+from utils import StudentIdentifyingInfo
+
+import credentials
+
+# StudentIdentifyingInfo Class
+ascend_students = StudentIdentifyingInfo()
+
+ascend_students.login_aspen_website(credentials.ASPEN_USERNAME, credentials.ASPEN_PASSWORD)
+ascend_students.select_view("school")
+ascend_students.select_school("bloom")
+ascend_report = ascend_students.build_report_students_tab()
+
+ascend_report.to_csv("ascend_report.csv")
